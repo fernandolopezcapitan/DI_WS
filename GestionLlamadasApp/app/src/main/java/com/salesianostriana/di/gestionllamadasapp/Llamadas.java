5f712b28582b8a26af00fa001b6f25eb3317c322
fernandolopezcapitan <lopezcapitan.fernando@gmail.com>
2015-10-27 11:35:08 +0100

    Actualizado

---
+++ GestionLlamadasApp/app/src/main/java/com/salesianostriana/di/gestionllamadasapp/Llamadas.java
@@ -0,0 +1,14 @@
+package com.salesianostriana.di.gestionllamadasapp;
+
+
+public class Llamadas {
+    private String nombre;
+    private int numero;
+    private String fecha;
+    private String restante;
+    private int flecha;
+
+
+
+
+}
