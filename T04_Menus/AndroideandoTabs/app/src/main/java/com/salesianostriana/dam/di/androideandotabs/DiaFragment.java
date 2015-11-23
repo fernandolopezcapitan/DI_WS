3f62f8f900d7de49a4e24dff5289f83c8fcab699
fernandolopezcapitan <lopezcapitan.fernando@gmail.com>
2015-11-23 20:42:07 +0100

    Articulo androideando Tabs

---
+++ T04_Menus/AndroideandoTabs/app/src/main/java/com/salesianostriana/dam/di/androideandotabs/DiaFragment.java
@@ -0,0 +1,30 @@
+package com.salesianostriana.dam.di.androideandotabs;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DiaFragment extends Fragment {
+
+
+    public DiaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dia, container, false);
+    }
+
+
+}
