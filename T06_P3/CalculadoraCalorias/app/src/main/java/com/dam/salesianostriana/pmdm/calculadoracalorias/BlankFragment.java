436cb8bac7922871b7e9ebdb4959b125d9c9fbde
fernandolopezcapitan <lopezcapitan.fernando@gmail.com>
2016-01-22 14:13:28 +0100

    calorias

--- T06_P3/CalculadoraCalorias/app/src/main/java/com/dam/salesianostriana/pmdm/calculadoracalorias/BlankFragment.java
+++ T06_P3/CalculadoraCalorias/app/src/main/java/com/dam/salesianostriana/pmdm/calculadoracalorias/BlankFragment.java
@@ -1,17 +1,29 @@
 package com.dam.salesianostriana.pmdm.calculadoracalorias;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.db.chart.model.BarSet;
+import com.db.chart.view.BarChartView;
+import com.db.chart.view.animation.Animation;
+import com.db.chart.view.animation.easing.SineEase;
+
+import java.text.DecimalFormat;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BlankFragment extends Fragment {
+
+
+    private final static String[] mLabels= {"W", "I", "L", "L", "I", "A", "M"};
+    private final static float[] mValues = {2.5f, 3.7f, 4f, 8f, 4.5f, 4f, 5f};
 
 
     public BlankFragment() {
@@ -23,7 +35,30 @@ public class BlankFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+
+        View v =  inflater.inflate(R.layout.fragment_blank, container, false);
+
+        BarChartView chart = (BarChartView) v.findViewById(R.id.stackBar);
+
+        BarSet dataset = new BarSet(mLabels, mValues);
+        dataset.setColor(Color.GREEN);
+        float start= (float) 10;
+        chart.setBarSpacing(start);
+        chart.addData(dataset);
+
+
+        chart.setLabelsFormat(new DecimalFormat('#' + " k/cal"));
+
+
+        Animation anim = new Animation(500);
+        anim.setEasing(new SineEase());
+        anim.setAlpha(3);
+
+        float start1= (float) 1.0;
+        anim.setStartPoint(start1, 0);
+        chart.show(anim);
+
+        return v;
     }
 
 }
--- T06_P3/CalculadoraCalorias/app/src/main/java/com/dam/salesianostriana/pmdm/calculadoracalorias/MapTrackerFragment.java
+++ T06_P3/CalculadoraCalorias/app/src/main/java/com/dam/salesianostriana/pmdm/calculadoracalorias/MapTrackerFragment.java
@@ -1,18 +1,28 @@
 package com.dam.salesianostriana.pmdm.calculadoracalorias;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.SphericalUtil;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -22,6 +32,7 @@ public class MapTrackerFragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap map;
     MapView mapView;
+    TextView txtMetros, txtCalorias;
 
     public MapTrackerFragment() {
         // Required empty public constructor
@@ -36,6 +47,9 @@ public class MapTrackerFragment extends Fragment implements OnMapReadyCallback {
 
         // Gets the MapView from the XML layout and creates it
         mapView = (MapView) v.findViewById(R.id.mapview);
+        txtMetros = (TextView) v.findViewById(R.id.Mapas_Distancia);
+        txtCalorias = (TextView) v.findViewById(R.id.Mapas_Calorias);
+
         mapView.onCreate(savedInstanceState);
 
         // Gets to GoogleMap from the MapView and does initialization stuff
@@ -61,10 +75,47 @@ public class MapTrackerFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng sydney = new LatLng(37.422134, -6.146573);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15));
+
+        final List<LatLng> lista_lat = new ArrayList<>();
+        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                final LatLng lat_long = new LatLng(latLng.latitude, latLng.longitude);
+                final Marker marcador = map.addMarker(new MarkerOptions()
+                        .position(lat_long)
+                        .draggable(true));
+
+                map.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                marcador.setTitle("Mi posicion");
+                marcador.showInfoWindow();
+
+                lista_lat.add(lat_long);
+                PolylineOptions options = new PolylineOptions();
+
+                for (int i = 0; i < lista_lat.size(); i++) {
+                    options.add(new LatLng(lista_lat.get(i).latitude, lista_lat.get(i).longitude))
+                            .width(15)
+                            .color(Color.BLUE);
+
+                    DecimalFormat decimalFormat = new DecimalFormat("##.##");
+                    double metros = SphericalUtil.computeDistanceBetween(lista_lat.get(0), lista_lat.get(lista_lat.size() - 1)) / 1000;
+                    //hace que se vaya mostrando la distancia recorrida en ese momento.
+                    txtMetros.setText(String.valueOf(decimalFormat.format(metros) + " km"));
+                    txtCalorias.setText(String.valueOf(decimalFormat.format(calcularCalorias(55, metros)) + " k/cal"));
+                }
+                Polyline polygon = map.addPolyline(options);
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(lat_long, 15));
+
+            }
+        });
+    }
+
+    public double calcularCalorias(double peso, double distancia){
+        return peso * distancia;
     }
 
 }
